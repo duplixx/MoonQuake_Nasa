d3952aa2213a165138d75083362183b7375809b2
duplix <duplixxx20@gmail.com>
2022-09-24 16:08:21 +0530

    .

--- src/App.js
+++ src/App.js
@@ -14,35 +14,6 @@ const App = () => {
 
   return (
     <>
-<<<<<<< HEAD
-    {loader === true ? <Preloader /> :
-      <div className="w-full bg-black h-screen">
-          <div className='home_page'>
-            {open ?
-            <Modal onClick={handleClick} /> : null
-            }
-            
-            </div>
-            <div className='absolute flex justify-start align-center z-10 h-screen '>
-             <SearchBar />
-            </div>
-            <Canvas className="">
-              <Suspense >
-            
-              <OrthographicCamera position={[0, 0, 0]} rotation={[0,10,0]} >
-              <OrbitControls autoRotate={false} enableZoom={false} autoRotateSpeed={0.6} minPolarAngle={Math.PI / 1.8} maxPolarAngle={Math.PI / 1.8} />
-                <ambientLight intensity="0.06" enableShadow={true}  dropShadows={true}/>
-                <directionalLight position={[190, 80, 10]} intensity={0.4}  />
-                  <Moon />
-                  <Stars/> 
-                  <Marks onClick={handleClick}/>
-                <Preload all />
-              </OrthographicCamera>
-              </Suspense> 
-            </Canvas>
-            
-      </div>
-=======
       {loader === true ? <Preloader /> :
         <BrowserRouter>
           <Routes>
@@ -56,7 +27,6 @@ const App = () => {
             />
           </Routes>
         </BrowserRouter>
->>>>>>> ca72510c01de2bcb18592382e72b71df3f355b8a
       }
     </>
   );
